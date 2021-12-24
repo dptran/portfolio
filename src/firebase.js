dcef5643cd16b8a86d589424c6505c492e019d63
dptran <darren-tran@hotmail.com>
2021-12-23 16:54:00 -0800

    firebase credentials updated

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,15 @@ import 'firebase/auth'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID,
-    measurementId: process.env.REACT_APP_MEASUREMENTID
+    apiKey: "AIzaSyDYrsCkvHn3fhPiIpZzaAZCEHFwkQCv9Js",
+    authDomain: "coding-temple-5c51a.firebaseapp.com",
+    projectId: "coding-temple-5c51a",
+    storageBucket: "coding-temple-5c51a.appspot.com",
+    messagingSenderId: "733434616246",
+    appId: "1:733434616246:web:a3e2f5d3cc9e87a5c7c917",
+    measurementId: "G-NYN5C5C4C2"
 };
 
-const firebase = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export default firebase;
